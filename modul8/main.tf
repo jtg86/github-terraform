b384719fc801e884e72116137b9d64932c0e5e72
jtg86 <jan_tommy_g@hotmail.com>
2023-11-01 11:44:27 +0100

    grs to lrs

--- modul8/main.tf
+++ modul8/main.tf
@@ -26,7 +26,7 @@ resource "azurerm_storage_account" "sa" {
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
-  account_replication_type = "GRS"
+  account_replication_type = "LRS"
   account_kind             = "StorageV2"
   static_website {
     index_document = "index.html"
